c930b1ce1f6c9cf4319c48b9e02f7c8e9768abf4
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-08-16 20:28:38 +0200

    Added route static html files

--- main.js
+++ main.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express()
 const port = 3000
 
-app.get('/', (req,res) => res.send('Mainpage'));
-app.get('/register', (req,res) => res.send('register'));
-app.get('/login', (req,res) => res.send('login'));
-app.get('/feed', (req,res) => res.send('feed'));
+let options = {
+  root: __dirname,
+  dotfiles: 'deny',
+  headers: {
+    'x-timestamp': Date.now(),
+    'x-sent': true
+  }
+}
+app.get('/', (req,res) => res.sendFile('pages/index.html',options));
+app.get('/register', (req,res) => res.sendFile('pages/register.html',options));
+app.get('/login', (req,res) => res.sendFile('pages/login.html', options));
+app.get('/feed', (req,res) => res.sendFile('pages/feed.html',options));
 
-
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
