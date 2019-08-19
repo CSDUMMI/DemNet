c730c7d290c16b5f9a0d5035f8e72cd4ce154d69
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-08-19 17:52:18 +0200

    Added authentification middleware

--- main.js
+++ main.js
@@ -1,50 +1,60 @@
 const express = require('express');
-const crypto = require('crypto');
-const fs = require('fs');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('cookie-parser');
 const app = express()
 const port = 3000
 
 let options = {
-  root: __dirname,
-  dotfiles: 'deny',
-  headers: {
-    'x-timestamp': Date.now(),
-    'x-sent': true
+  root : __dirname
+};
+
+app.use(cookieParser());
+app.use(express.static('public'));
+
+let logged_in = {};
+
+function authenticate(req,res,next) {
+  name = req.cookies.name;
+  key = req.cookies.auth;
+  if ( logged_in[name] == key && key != undefined && name != undefined ) {
+    next();
+  } else {
+    res.redirect("/login");
   }
 }
 
 
-function load_login_info(user) {
-  // Return SHA256 of pass of user
-}
-
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded( { extended: true })) // for parsing application/x-www-form-urlencoded
-
 app.get('/', (req,res) => {
+  console.log("GET /");
   res.sendFile('pages/index.html',options)
 });
 
 app.get('/register', (req,res) => {
+  console.log("GET /register")
   res.sendFile('pages/register.html',options);
 });
 
 app.get('/login', (req,res) => {
+  console.log("GET /login");
   res.sendFile('pages/login.html', options);
 });
 
 app.post('/login', (req,res) => {
-  if (authenticate(req.body['name'],req.body['pass'])) {;
-    res.
-  }
+  console.log("POST /login");
+  // Load information (SHA256 of User pass)
+  let name = req.body.name;
+  let pass = req.body.pass; // SHA256 of the password
+  let stored_pass = load_user_pass(name); // SHA256(SHA256(pass))
+  if( pass == stored_pass ) {
 
-  res.send("LOGGED IN");
-});
+  }
+})
+app.use(authenticate);
 
 app.get('/feed', (req,res) => {
+  console.log("GET /feed")
   res.sendFile('pages/feed.html',options);
 });
 
-app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
