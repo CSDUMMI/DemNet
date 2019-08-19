9b1c8a0751cb9002f551ef743a2906a3074e0961
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-08-19 18:50:41 +0200

    Added lib.js

---
+++ lib.js
@@ -0,0 +1,27 @@
+const crypto = require('crypto');
+/*
+Personal function library
+*/
+
+// Object that can't be touched by main.js or anyother library
+let logged_in = {};
+
+
+// All routes after this middleware are only accessible for logged in users
+function authenticate(req,res,next) {
+  name = req.cookies.name;
+  key = req.cookies.auth;
+  if ( logged_in[name] == key && key != undefined && name != undefined ) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+}
+
+function login (name,pass) {
+  
+}
+
+module.exports = {
+  login : login
+}
--- main.js
+++ main.js
@@ -1,28 +1,19 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
+
+// Own module
+const lib = require('lib');
 
 let options = {
   root : __dirname
 };
 
+app.use(bodyParser());
 app.use(cookieParser());
 app.use(express.static('public'));
-
-let logged_in = {};
-
-function authenticate(req,res,next) {
-  name = req.cookies.name;
-  key = req.cookies.auth;
-  if ( logged_in[name] == key && key != undefined && name != undefined ) {
-    next();
-  } else {
-    res.redirect("/login");
-  }
-}
-
 
 app.get('/', (req,res) => {
   console.log("GET /");
@@ -41,15 +32,15 @@ app.get('/login', (req,res) => {
 
 app.post('/login', (req,res) => {
   console.log("POST /login");
-  // Load information (SHA256 of User pass)
-  let name = req.body.name;
-  let pass = req.body.pass; // SHA256 of the password
-  let stored_pass = load_user_pass(name); // SHA256(SHA256(pass))
-  if( pass == stored_pass ) {
 
-  }
+  let name = req.body.name;
+  let pass = req.body.pass;
+
+  let auth = lib.login(name,pass);
+  res.cookie("auth",auth);
 })
-app.use(authenticate);
+
+app.use(lib.authenticate);
 
 app.get('/feed', (req,res) => {
   console.log("GET /feed")
