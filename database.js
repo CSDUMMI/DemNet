b70792d66102bf9c6c5314654b08d153f6cd591a
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-09-02 18:31:59 +0200

    add_content: Add content to user get_feed: Get feed content for a user

--- database.js
+++ database.js
@@ -1,20 +1,56 @@
 const fs = require('fs');
 
-const db = JSON.parse(fs.readFileSync("database.alpha.json"));
+const db = JSON.parse( fs.readFileSync( "database.alpha.json" ) );
 const save_point = 0;
 process.env.CHANGES = process.env.CHANGES | 10;
 
-function password_of(name) {
+function password_of( name ) {
   return db.users[name].pass;
 }
 
-function create_user(username,email,password_hash) {
+function create_user( username, email, password_hash ) {
   if ( save_point >= process.env.CHANGES) save();
   if (db.users.hasOwnProperty(username)) res.redirect('/register');
   db.users[username] = {
     'pass'      : password_hash,
     'email'     : email,
-    'content'   : []
+    'content'   : [],
+    'followed'  : [],
+    'feed'      : []
+  }
+}
+
+/*
+add_content created by username.
+After content has been added,
+all followers feeds gets a reference
+to the content.
+*/
+function add_content( username, content ) {
+  if( db.users.hasOwnProperty( username ) ) {
+    content_id = {
+      'author'  : username,
+      'index'      : db.users[username].length
+    }
+
+    db.users[username].content.push( content );
+
+    for( follower in db.users[username].followed ) {
+      db.users[follower].feed.push( content_id );
+    }
+  }
+}
+
+/*
+get_feed:
+Return string array of all contents, relevant
+for the username.
+It returns max contents
+*/
+function get_feed( username, max=256 ) {
+  raw_feed = [];
+  for ( content in db.users[username].feed ) {
+    raw_feed.push( db.users[ content.author ].content[ content.index ] );
   }
 }
 
@@ -22,8 +58,8 @@ function create_user(username,email,password_hash) {
 function save() {
   // Save the db in json
   db_string = JSON.stringify ( db );
-  fs.writeFile("database.alpha.json", db_string, ( error ) => {
-    if ( error ) console.error(`Error: Writing to File:\n${ error }`);
+  fs.writeFile( "database.alpha.json", db_string, ( error ) => {
+    if ( error ) console.error( `Error: Writing to File:\n${ error }` );
     console.log("Saved state");
   });
 }
@@ -31,5 +67,5 @@ function save() {
 module.exports = {
   password_of : password_of,
   create_user : create_user,
-  key : process.env.SECRET,
+  key         : process.env.SECRET,
 }
