ec51c55c65991667f652d8573434ce974c7cf485
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-09-09 16:57:23 +0200

    PORT -> port

--- serving.js
+++ serving.js
@@ -4,12 +4,20 @@ const express = require('express');
 const app       = express();
 const port      = process.env.PORT;
 
-app.get('/', ( req, res ) => {
+app.get( '/', ( req, res ) => {
   res.sendFile( '/pages/index.html' );
 });
 
-app.get('/feed', ( req, res ) => {
+app.get( '/feed', ( req, res ) => {
   res.sendFile( '/pages/index.html' );
 });
 
-app.listen(port, ()  => console.log(  `Serving files on 0.0.0.0:${PORT}`  ) );
+app.get( '/login', ( req, res ) => {
+  res.sendFile( '/pages/login.html' );
+});
+
+app.get( '/register', ( req, res ) => {
+  res.sendFile( '/pages/register.html' );
+}
+
+app.listen(port, ()  => console.log(  `Serving files on 0.0.0.0:${port}`  ) );
