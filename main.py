d861cbce2cee11174e8c2fec6172c141d5606ac8
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-09-22 13:08:36 +0200

    New URI Route /info to associate user id to username and possibly other things

--- main.py
+++ main.py
@@ -17,10 +17,29 @@ def index():
     if 'logged_in' in session and 'userId' in session:
 
         ( connection, cursor ) = connect()
-        
+
         userId = session [ 'userId' ]
         # SELECT all contents from all people userId is following
         query_feed = """
 SELECT * FROM contents WHERE author_id IN  ( SELECT follower FROM followers WHERE follower = ? )
         """
         cursor.execute( query_feed, [ userId ] )
+        feed = cursor.fetchall()
+        return render_template( 'feed.html', feed )
+    else:
+        return render_template( 'startpage.html' )
+
+@app.route( "/id_to", methods=[ "POST" ] )
+def info():
+    ( connection, cursor )  = connect()
+
+    types                   = {
+        "name"      : "name"
+    }
+
+    type                    = type if types[ type ] else "ERROR"
+    id                      = request.form[ 'id' ]
+    query_feed              = "SELECT ? FROM users WHERE id = ?"
+    cursor.execute( query_feed, type, id )
+    answer                  = cursor.fetchone()
+    return make_response( JSON.stringify( answer ) )
