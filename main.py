6af0b9d0d4233afbf5d54e52db17caf714008966
root <root@debian.debian.org>
2019-09-26 19:15:08 +0200

    Inlight of the certain change from a centrelized network to an IPFS powered decentrelized one, both all code relating soley to the SQLite DB and the DB itself are removed.

--- main.py
+++ main.py
@@ -1,45 +1,11 @@
 #! /bin/usr/python3.5
-from flask import Flask, request, render_template, send_from_directory, redirect, session
+from flask import Flask, request, render_template, send_from_directory, redirect, session, json
 from Crypto.Hash import SHA256
-import sqlite3 as sqlite
-import os
+import ipfshttpclient as ipfs
 
 app            = Flask( __name__, static_folder = 'static', static_url_path = '' )
 app.secret_key = Random.new( os.environ [ "SEED" ] ).read(16)
 
 def connect():
-    con = sqlite.connect( 'data.db' )
-    cursor = con.cursor()
-    return ( con, cursor )
-
-@app.route ( "/" )
-def index():
-    if 'logged_in' in session and 'userId' in session:
-
-        ( connection, cursor ) = connect()
-
-        userId = session [ 'userId' ]
-        # SELECT all contents from all people userId is following
-        query_feed = """
-SELECT * FROM contents WHERE author_id IN  ( SELECT follower FROM followers WHERE follower = ? )
-        """
-        cursor.execute( query_feed, [ userId ] )
-        feed = cursor.fetchall()
-        return render_template( 'feed.html', feed )
-    else:
-        return render_template( 'startpage.html' )
-
-@app.route( "/id_to", methods=[ "POST" ] )
-def info():
-    ( connection, cursor )  = connect()
-
-    types                   = {
-        "name"      : "name"
-    }
-
-    type                    = type if types[ type ] else "ERROR"
-    id                      = request.form[ 'id' ]
-    query_feed              = "SELECT ? FROM users WHERE id = ?"
-    cursor.execute( query_feed, type, id )
-    answer                  = cursor.fetchone()
-    return make_response( JSON.stringify( answer ) )
+    # Connect to the IPFS daemon running on the server on localhost!
+    return ipfs.connect( '/ip4/127.0.0.1/tcp/5001/http' )
