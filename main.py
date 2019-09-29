89f1161ad0a3692b34904ef429eec42632b18bdd
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-09-29 20:43:07 +0200

    get_users_data() loads User Information from IPFS

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 #! /bin/usr/python3.5
-from flask import Flask, request, render_template, send_from_directory, redirect, session, json
-from Crypto.Hash import SHA256
+import json
 import ipfshttpclient as ipfs
 
-app            = Flask( __name__, static_folder = 'static', static_url_path = '' )
-app.secret_key = Random.new( os.environ [ "SEED" ] ).read(16)
+app             = Flask( __name__, static_folder = 'static', static_url_path = '' )
+app.secret_key  = Random.new( os.environ [ "SEED" ] ).read(16)
 
+users_index     = os.environ[ "USER_INDEX" ]
+
+"""
+Connect to IPFS Daemon
+"""
 def connect():
     # Connect to the IPFS daemon running on the server on localhost!
     return ipfs.connect( '/ip4/127.0.0.1/tcp/5001/http' )
+
+"""
+get_users_data:
+Load List of JSON Objects from
+IPFS
+"""
+def get_users_data():
+    client = connect()
+    users  = client.cat( users_index )
+    users  = users.split( "\n" )
+
+    for i in range( len( users ) ):
+        users[i] = client.cat ( users[i] )
+        users[i] = json.loads( users[i] )
+
+    return users
+
+"""
+Initialize election,
+thus making this server
+the supervisor of the election.
+"""
+def init_election():
+        get_users_data()
