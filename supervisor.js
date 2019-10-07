b7753018bddfd6a22437318735fc4cb68b2c5d24
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-10-07 20:27:25 +0200

    Before making a fully functional Web App, I will try to build a CLI.

--- supervisor.js
+++ supervisor.js
@@ -8,22 +8,69 @@ const fs      = require( 'fs' );
 const crypto  = require( 'crypto' );
 const express = require( 'express' );
 const cookieParser = require( 'cookie-parser' );
-const session = require( 'express-session' );
+const bodyParser = require( 'body-parser' );
+const cookieSession = require( 'cookie-session' );
 const app     = express();
 
-
 app.use( cookieParser() );
-app.use( session( { secret : process.env.SECRET } ) );
+app.use( bodyParser() );
+app.use( cookieSession( {
+  name: 'session',
+  keys: process.env.SECRET
+} ) );
 
 const users = JSON.parse( fs.readFileSync( 'users.json' ) );
 const elections = JSON.parse( fs.readFileSync( 'elections.json' ) );
 
-app.set( 'view-engine', 'ejs' );
+const PORT = 3000;
 
-app.get( '/', async ( req, res ) => {
-  if( req.session.looged_in ) {
-    res.render( 'home', { messages : users[ req.session.user_id ].messages } );
-  } else {
+app.set( 'view engine', 'ejs' );
+
+app.get( '/', ( req, res ) => {
+    console.log( 'GET index' );
     res.render( 'index' );
+});
+
+app.get( '/home', ( req, res ) => {
+  console.log( 'GET home' );
+  if( req.session.username ) {
+    const username = req.body.username;
+
+    res.render( 'home', { messages : users[ username ].messages, name : username } );
+  } else {
+    res.redirect( '/' );
   }
-})
+});
+
+app.post( '/register', ( req, res ) => {
+  if( req.session.username ) {
+    res.redirect( '/' );
+  } else {
+
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = crypto.createHmac('sha256', process.env.SUPER_SECRET ).update( req.body.password );
+
+    console.log( 'POST /register' );
+    if( !( username in users ) ) {
+
+      users[ username ] = {
+        'email'     : email,
+        'password'  : password.digest( 'hex' ),
+        'messages'  : [
+          {
+            title   : "Welcome",
+            author  : "DemNet Team",
+            content : 'Welcome to DemNet. Hope you will participate in the next vote.'
+          }
+        ]
+      };
+      req.session.username = username;
+      res.redirect( '/home' );
+    }
+
+    res.redirect( '/' );
+  }
+} );
+
+app.listen( PORT, () => console.log( `Listening on ${PORT}` ) );
