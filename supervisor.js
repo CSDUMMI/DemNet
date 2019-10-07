d31c5dde5179e440ca9adf35af504cf2292a7b0c
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-10-07 18:16:19 +0200

    Installed Dependencies. Simple Homepage

--- supervisor.js
+++ supervisor.js
@@ -4,19 +4,26 @@ logging in users, counting votes,
 publishing results.
 */
 
+const fs      = require( 'fs' );
 const crypto  = require( 'crypto' );
 const express = require( 'express' );
 const cookieParser = require( 'cookie-parser' );
 const session = require( 'express-session' );
 const app     = express();
 
+
 app.use( cookieParser() );
 app.use( session( { secret : process.env.SECRET } ) );
 
 const users = JSON.parse( fs.readFileSync( 'users.json' ) );
 const elections = JSON.parse( fs.readFileSync( 'elections.json' ) );
+
+app.set( 'view-engine', 'ejs' );
+
 app.get( '/', async ( req, res ) => {
   if( req.session.looged_in ) {
-    res.send( )
+    res.render( 'home', { messages : users[ req.session.user_id ].messages } );
+  } else {
+    res.render( 'index' );
   }
 })
