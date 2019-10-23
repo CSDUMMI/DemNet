4a789be94b838abac64603b4b454671bb6ae95a8
joris <joris@blocked.blocked.org>
2019-10-23 17:54:14 +0200

    Removed Testing

--- Server/main.py
+++ Server/main.py
@@ -1,10 +1,11 @@
 import json
 
-def count_votes( votes, particpant_count, options ):
+def count_votes( votes, participant_count, options ):
     ballot = map( lambda option : { "option" : option, "support" : list() }, options )
-    return _count_votes( votes, particpant_count, list(ballot) )
+    return _count_votes( votes, participant_count, list(ballot) )
 
-def _count_votes( votes, particpant_count, ballot ):
+
+def _count_votes( votes, participant_count, ballot ):
     # Base case
     if len(ballot) == 1:
         return ballot[0]
@@ -20,7 +21,7 @@ def _count_votes( votes, particpant_count, ballot ):
     ballot = sorted( ballot, key=lambda option: len(option["support"]), reverse=True )
 
     # If any option got more than 50% of the support, that is the winner.
-    if len( ballot[0]['support'] ) > particpant_count/2:
+    if len( ballot[0]['support'] ) > participant_count/2:
         return ballot[0]
     else:
         # Eliminate least popular option and recount those results
@@ -29,18 +30,28 @@ def _count_votes( votes, particpant_count, ballot ):
         looser = list(map( lambda v: v[:-1], looser['support']))
 
         print( looser )
-        return _count_votes( looser, particpant_count, ballot )
+        return _count_votes( looser, participant_count, ballot )
+
+
+
+
+
+
 # Expected Winner = C
 votes = [
             [ 'C', 'B', 'A' ],
             [ 'C', 'B', 'A' ],
             [ 'C', 'B', 'A' ],
-            [ 'A', 'B', 'C' ],
-            [ 'A', 'B', 'C' ],
-            [ 'A', 'B', 'C' ],
-            [ 'B', 'A', 'C' ]
+            [ 'A', 'C', 'B' ],
+            [ 'C', 'A', 'B' ],
+            [ 'A', 'C', 'B' ],
+            [ 'A', 'B', 'C' ]
+
         ]
 
 result =  count_votes( votes, len( votes ), [ 'A', 'B', 'C' ] )
 
-print( f"Winner { result['option'] } with { round( len( result['support'] )/len(votes) * 100) }%")
+if result == "ERROR":
+    print( "Malformatted votes, options or participant_count" )
+else:
+    print( f"Winner { result['option'] } with { round( len( result['support'] )/len(votes) * 100) }%")
