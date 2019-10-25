ff62fbdf1b017298aca44b6c2db32131893d0b86
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-10-25 21:12:17 +0200

    elm teast

---
+++ src/Main.elm
@@ -0,0 +1,42 @@
+module Main exposing (..)
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+-- Main
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+-- Model
+
+type alias Model = Int
+
+init : Model
+init =
+  0
+
+
+-- update
+
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+-- view
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (String.fromInt model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
