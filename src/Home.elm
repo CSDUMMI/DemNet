b8abd6cf1efa435a6bd4c905c3f700d7543c2d07
Joris Gutjahr <joris.gutjahr@protonmail.com>
2019-10-29 18:36:14 +0100

    Cycling through a list. TODO: CSS Design

--- src/Home.elm
+++ src/Home.elm
@@ -5,6 +5,7 @@ import Time
 import Viewing exposing (..)
 import Array
 import Delay
+import Cycle
 
 main = Browser.element
         {  init = init
@@ -19,38 +20,30 @@ descriptions =  Array.fromList
                 , "The democratic social network"
                 ]
 
-type alias Model =  { queue : List String
-                    , current : String
-                    }
+type alias Model =  { cycle : Cycle.Cycle String }
 
 init : () -> (Model, Cmd Msg)
-init _ = ({ queue = [ "Transparent"
-          , "Free"
-          , "Libre"
-          , "The democratic social network"]
-          , current = "Democratic"
-          }, cycleDescriptions )
-type Msg = Change Time.Posix
+init _ = ({ cycle = Cycle.init "Democratic"
+                [ "Transparent"
+                , "Free"
+                , "Libre"
+                , "The democratic social network"
+                ]
+         }, Cmd.none )
+type Msg = Change
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Change _ -> let newCurrent  = case List.head model.queue of
-                      Just c -> c
-                      Nothing -> model.current
-                    newQueue    = model.queue ++ [model.current]
-                in  ( { current = newCurrent
-                      , queue   = newQueue
-                      }, Cmd.none )
+    Change -> ({ model | cycle = Cycle.step model.cycle }, Cmd.none )
 
 -- SUBSCRIPTIONS
 subscriptions : Model -> Sub Msg
-subscriptions model =
-  Time.every Time.second Change
+subscriptions model = Time.every 4000 (\_ -> Change)
 
 view : Model -> Html Msg
 view model =
   div []
     [ viewHeader
-    , text model.current
+    , text (Cycle.next model.cycle)
     ]
