3e51669c60214692174d87f6ca33774f5b7818c8
Joris Gutjahr <joris.gutjahr@protonmail.com>
2019-11-03 16:05:02 +0100

    Reduced Scope, instead of using Browser.application, using Browser.element

--- src/Main.elm
+++ src/Main.elm
@@ -15,44 +15,19 @@ import Requests
 import Cycle
 
 -- MAIN
-main = Browser.application
+main = Browser.element
   { init = init
   , view = view
   , update = update
   , subscriptions = subscriptions
-  , onUrlRequest = LinkClicked
-  , onUrlChange = UrlChanged
   }
 
 -- MODEL
-type alias Model
-  = { main_page : Main_Page
-    , navigation : Nav_Items
-    }
+type Model
+  = Navigation
+
 
 -- UPDATE
 type Msg
-  = Save_Post Post.Post
-  | Change_Main_Page Main_Page
-  | Writing_Publish
-  |
--- SUBSCRIPTIONS
-subscriptions : Model -> Sub Msg
-subscriptions model = Sub.none
-
--- VIEW
--- views:
--- NavBar (Different Nav_Items, depending on the Context )
--- User ( User's Name and Image )
--- Feed ( Feed of Messages send to the user )
--- Actions (Actionsbar on the right side, a collection of actions, such as edit, publish, logout, etc.)
--- Editing (Edit and publish a Post)
--- Reading (Read a Post)
--- Login
--- Register
-
-view : Model -> Browser.Document Msg
-view (Model model) =
-  { title = model.title
-  , body = Element.layout [] [Element.text "Hello"]
-  }
+  = LinkClicked Browser.UrlRequest
+  | UrlChanged Url.Url
