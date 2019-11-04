57ea012864500c3404522dc02767d6dcea1d285d
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-11-04 16:44:05 +0100

    Defined save_post and publish_post using a general upload function.

--- src/Requests.elm
+++ src/Requests.elm
@@ -1,4 +1,18 @@
-module Requests exposing ( save_post )
+module Requests exposing ( save_post
+                         , publish_post
+                         )
 
-save_post : post -> Cmd msg
-save_post _ = Cmd.none
+import Http
+
+save_post : msg -> post -> Cmd msg
+save_post = upload "/save"
+
+publish_post : msg -> post -> Cmd msg
+publish_post = upload "/publish"
+
+upload : String -> msg ->  post -> Cmd msg
+upload url expect { title, content }
+  = Http.post { url = url
+              , body = Http.stringBody "plain/text" ( "# " ++ title ++ "\n" ++ content)
+              , expect = Http.expectString expect
+              }
