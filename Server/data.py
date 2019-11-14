5bbdeab74c169c9b25e36c361e054a9985b4e8a4
Joris Gutjahr <joris.gutjahr@protonmail.com>
2019-11-14 19:05:30 +0100

    Skeleton

--- Server/data.py
+++ Server/data.py
@@ -2,3 +2,15 @@
 # - Store data as JSON Format
 # - Save it to disk if necessary (try.. expect)
 # - wait or reqests on local server ( flask )
+
+try:
+    from flask import Flask, request, jsonify
+    app = Flask(__name__)
+    save_file = open("demnet.data", "w+")
+
+    # Data Model.
+    # Everything is a JSON Object.
+except Exception as e:
+    raise
+else:
+    pass
