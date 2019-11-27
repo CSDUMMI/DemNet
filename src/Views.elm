766b8a8c9631cd01b151efc3648cd6ec65ae8e8c
Joris Gutjahr <joris.gutjahr@protonmail.com>
2019-11-27 14:08:28 +0100

    Implement simple Element.textColumn to create a post view.

--- src/Views.elm
+++ src/Views.elm
@@ -5,5 +5,10 @@ import Element exposing ( Element )
 import Element.Background as Background
 import Requests exposing ( Post )
 
-view_post : Post -> (String -> Element) Element
-view_post post toElement = 
+view_post : Post -> (String -> Element) -> (String -> Element) -> Element
+view_post post fromTitle fromContent =
+  Element.textColumn post_attr
+    [ fromTitle post.title
+    , fromContent post.content
+    , additional_fields
+    ]
