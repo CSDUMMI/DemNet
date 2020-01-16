703b2d3f3aa07f82c534a3fcdd1557f2cdb6eb1b
Joris Gutjahr <joris.gutjahr@protonmail.com>
2020-01-16 17:40:58 +0100

    Checked if Patch exists, before merging.

--- Server/patch.sh
+++ Server/patch.sh
@@ -6,19 +6,27 @@
 # this script is called again and merged with the origin repository.
 #
 
+# bash patch.sh create <origin_repo --bare> <patcher's name> <patch name>
 if [ $1 = "create" ]
 # $2 = Path of Origin Repository
 # $3 = Name of the Patcher (all without whitespaces)
 # $4 = Name of the Patch (all without whitespaces)
 then
   git clone $2 $PATCHES/"$3-$4"
+# bash patch.sh merge <patcher's name> <patch name>
 elif [ $1 = "merge" ]
 # $2 = Name of the Patcher (without whitespaces)
 # $3 = Name of the Patch
 then
   current_pwd=pwd
-  cd $PATCHES/"$2-$3"
-  git push
+  location=$PATCHES/"$2-$3"
+  if [ -e $location ]
+    then
+      cd $location
+      git push
+    else
+      echo "No Patch with that name"
+  fi
 else
   echo "Please provide a command"
 fi
