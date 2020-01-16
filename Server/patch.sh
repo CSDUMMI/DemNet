0ebc31826626dd2f8bb949ad14a09e970ef9c984
Joris Gutjahr <joris.gutjahr@protonmail.com>
2020-01-16 17:22:37 +0100

    Removed whitespaces. Manual tests passed.

--- Server/patch.sh
+++ Server/patch.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 
-
-
-
-
-if [$1 = "create"]
+if [ $1 = "create" ]
 # $2 = Path of Origin Repository
 # $3 = Name of the Patcher (all without whitespaces)
 # $4 = Name of the Patch (all without whitespaces)
 then
   git clone $2 $PATCHES/"$3-$4"
-elif [$1 = "merge"]
-# $2 = Path of the Origin Repository
-# $3 = Name of the Patcher (without whitespaces)
-# $4 = Name of the Patch
+elif [ $1 = "merge" ]
+# $2 = Name of the Patcher (without whitespaces)
+# $3 = Name of the Patch
 then
   current_pwd=pwd
-  cd $PATCHES/"$3-$4"
+  cd $PATCHES/"$2-$3"
   git push
 else
   echo "Please provide a command"
+fi
