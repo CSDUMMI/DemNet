b1d6a75e56b837415cc476f541a043dfb03ed872
Joris Gutjahr <joris.gutjahr@protonmail.com>
2020-01-16 21:02:45 +0100

    Added Elections.py Module, to better handle and organize the interaction between: 1. elections.py Module 2. MongoDB 3. The Users.

---
+++ Server/Elections.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import Patches
+from election import count_votes
+from pymongo import MongoClient
+
+"""
+To create, count and handle victory conditions.
+
+There are two kinds of elections,
+as there are two kinds of laws:
+Executable and Human Readable.
+
+Executable Law is Source Code,
+that must be just as approved as Textual, Human Readable to be more percise,
+Law. But they have to be handled differently on this level.
+"""
+def create(type,options):
+    if type == "human-readable":
+
+    elif type == "executable":
+
+    else:
+        print("Error: Invalid type in Election")
