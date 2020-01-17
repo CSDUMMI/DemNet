ee396d24d94019408d59ea41db42bedd2acbec9d
Joris Gutjahr <joris.gutjahr@protonmail.com>
2020-01-17 16:39:45 +0100

    Added Documentation

--- Server/Elections.py
+++ Server/Elections.py
@@ -15,9 +15,39 @@ Executable and Human Readable.
 Executable Law is Source Code,
 that must be just as approved as Textual, Human Readable to be more percise,
 Law. But they have to be handled differently on this level.
+
+Human-readable laws are:
+- Changes to right/wrong questions (laws in the juristical sense)
+- Proposal for a Patch (pre-election)
+
+Executable laws are:
+- Fully developed patches, that must be voted on.
+
+# All changes, in general
+All changes change laws and depending on
+whether those laws are executable or human-readable
+they have to differ somewhat.
+These fields they have in general though:
+- simple_description, simple, brief language
+to get an overview of the changes
+- long_description, conclusive, may even complex, description.
+A bad long_description is one that is written with the intent
+to promote the change.
+
+# Human-Readable Changes
+Human Readable Laws consist of paragraphs.
+If the Changes have human-readable laws the field
+"human-readable" is filled with an array of Law elements:
+This can be of three kinds:
+- "Remove Law" + <law_id>
+- { "id" : SHA256(this)
+  , "paragraphs" : ["<§1>","<§2>",etc.]
+  ,
+  }
 """
 def create(type,options):
     if type == "human-readable":
+
     elif type == "executable":
 
     else:
