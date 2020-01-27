334f854a6733be85154f2ee46f32f4a963fc6a92
abbashan03 <abbashankarasahin@gmail.com>
2020-01-27 18:55:10 +0100

    Added INIT and UPDATE part.

--- src/Main.elm
+++ src/Main.elm
@@ -5,4 +5,57 @@ import Browser
 import Element exposing (Element)
 import Http
 
-main = Element.layout [] <| Element.text "Hello"
+main = Browser.document { init = init
+                        , view = view
+                        , update = update
+                        , subscription = subscription
+                        }
+
+-- MODEL
+type alias User = { username : String
+                  , first_name : String
+                  , last_name : String
+                  }
+
+type alias Message
+  = { from : User
+    , to : User
+    , title : String
+    , content : String
+    }
+
+type Page
+  = Reading Message
+  | Writing Message
+  | Feed (List Message)
+
+type alias Model =  { user      : Maybe User
+                    , page      : Page
+                    , readings  : List Message
+                    , writings  : List Message
+                    , feed      : List Message
+                    }
+
+init : flags -> ( Model, Cmd Msg)
+init _ = ( { user = Nothing
+           , readings = []
+           , writings = []
+           , feed = []
+           }, Cmd.none )
+
+-- UPDATE
+type Msg
+  = Read Message
+  | Write Message
+  | Feed
+  | Write_Change_Title String
+  | Write_Change_Content String
+  | Login_Username_Change String
+  | Login_Password_Change String
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model
+  = case model.user of
+      Just user -> case msg of
+        Read msg ->
+      Nothing   ->
