720363901c940130f9d175d9dc4d660c1cd52fe1
Joris Gutjahr <joris.gutjahr@protonmail.com>
2020-02-08 16:48:17 +0100

    Solved a bug about how self.__resort pops the last element of the list automatically, thus deleting the first choice of the vote, if called on the original vote.

--- Server/election.py
+++ Server/election.py
@@ -14,8 +14,10 @@ are resorted into the new options.
 class Turn():
     def __init__(self,participants : int, votes : List[Vote], options : List[Option], fs : TextIO = None):
         if len(votes) < participants:
-            votes.extend([["NoneOfTheOtherOptions"] for x in range(participants-len(votes))])
-        self.__options__ = { key : [] for key in options }
+            votes.extend([["NoneOfTheOtherOptions","NoneOfTheOtherOptions"] for x in range(participants-len(votes))])
+
+
+        self.__options__ = { key : list(filter(lambda v:v[-1] == key, votes)) for key in options }
 
         if fs != None:
             self.result_file = fs
@@ -24,7 +26,7 @@ class Turn():
 
         print(f"Votes:\n {votes}\nThrown:", file=self.result_file)
 
-        self.__resort(votes)
+
         self.participants = participants
 
 
@@ -34,7 +36,7 @@ class Turn():
 
             old = vote.pop()
             if old == "NoneOfTheOtherOptions":
-                break
+                continue
             elif len(vote) == 0:
                 self.__options__["NoneOfTheOtherOptions"].append(["NoneOfTheOtherOptions"])
             else:
@@ -48,8 +50,13 @@ class Turn():
     Either
     - (winner_name,support) : Tuple[str,List[Vote] - If one option has won
     - [(one_winner,support),(other_winner,support)] : List[Tuple[str,List[Vote]]] - If no side could unite the necessary support
+    - False - If Error has occured and invalid data been provided.
     """
     def count(self, threshold : float =0.5):
+        # If threshold were less than 50%, there could be multiple
+        # winners, which is impossible under this setup.
+        if threshold < 0.5 or threshold > 1:
+            return False
         winner = None
         while winner == None:
             least = []
@@ -57,12 +64,13 @@ class Turn():
                 option = (option,len(self.__options__[option]))
                 if option[1] > self.participants*threshold:
                     winner = option
-                elif len(self.__options__) == (1/limit): # another election must be called.
+                elif len(self.__options__) == 2: # another election must be called.
                     keys = list(self.__options__)
                     winner = [(key,self.__options__[key]) for key in keys]
                 elif option[1] < sum([s[1] for s in least]):
                     least = option
                 elif option[1] == sum([s[1] for s in least]):
+                    print(self.__options__)
                     least.append(option)
 
 
