8c77aa29d4ba06d239ddb45250a8fb317e087848
Joris Gutjahr <joris.gutjahr@protonmail.com>
2020-02-08 11:09:36 +0100

    Added mechanic for log file in election_example.py

--- Server/election_example.py
+++ Server/election_example.py
@@ -14,26 +14,18 @@ def load_sample_votes():
 
 def generate_election( options, participants ):
     votes = []
+    result_file = open("sample_election.el","r+")
     while( participants > 0 ):
         votes.append( generate_random_vote(options) )
         participants -= 1
 
-    winner = count_votes( len(votes), votes, options )
+    winner = count_votes( len(votes), votes, options, fs=result_file )
 
-    winners = []
-    for i in range(len(options)):
-        votes_in_i = list(map( lambda vote: vote[-i],votes ))
-        winner_votes = votes_in_i.count(winner['ballot']['option'])
-        winners.append( ( winner_votes, len(votes_in_i) ))
 
-    print(f"Result:{winner['ballot']['option']}")
 
-    for i in range(len(winners)):
-        percentage = round((winners[i][0]/winners[i][1]) * 100, ndigits=2)
-        print(f"Winner' votes in Vote #{i}:\t{winners[i][0]} of {winners[i][1]},\t{ percentage }% ")
 
 if __name__ == '__main__':
     i = 10
     while(i > 0):
-        generate_election(['A','B','C','D', 'E', 'F', 'G', 'H', 'I'],random.randint(10**2,10**3))
+        generate_elections(['A','B','C','D', 'E', 'F', 'G', 'H', 'I'],random.randint(10**2,10**3))
         i -= 1
