8580baa13026bb4b72220c321684f4b2d2bdee6e
Joris Gutjahr <joris.gutjahr@protonmail.com>
2020-02-08 11:08:36 +0100

    Added Log File mechanic

--- Server/election.py
+++ Server/election.py
@@ -1,5 +1,5 @@
-import functools
-from typing import List, Tuple
+from typing import List, Tuple, TextIO
+import sys
 
 Vote = List[str]
 Option = str
@@ -12,18 +12,33 @@ If an option is deleted, the votes removed from it
 are resorted into the new options.
 """
 class Turn():
-    def __init__(self,participants : int, votes : List[Vote], options : List[Option]):
+    def __init__(self,participants : int, votes : List[Vote], options : List[Option], fs : TextIO = None):
+        if len(votes) < participants:
+            votes.extend([["NoneOfTheOtherOptions"] for x in range(participants-len(votes))])
         self.__options__ = { key : [] for key in options }
+
+        if fs != None:
+            self.result_file = fs
+        else:
+            self.result_file = sys.stdout
+
+        print(f"Votes:\n {votes}\nThrown:", file=self.result_file)
+
         self.__resort(votes)
         self.participants = participants
 
 
+
     def __resort(self,votes : List[Vote]):
         for vote in votes:
-            vote_old = vote
-            print(f"Changed from {vote.pop()} to {vote[-1]} in {vote_old}")
 
-            self.__options__[vote[-1]].append(vote)
+            old = vote.pop()
+            if old == "NoneOfTheOtherOptions":
+                break
+            elif len(vote) == 0:
+                self.__options__["NoneOfTheOtherOptions"].append(["NoneOfTheOtherOptions"])
+            else:
+                self.__options__[vote[-1]].append(vote)
 
     def count(self):
         winner = None
@@ -39,12 +54,13 @@ class Turn():
                     least.append(option)
 
             for s in least:
+                print(f"{s[0]}",file=self.result_file)
                 self.__resort(self.__options__[s[0]])
-                print(s[0])
-                del self.__options__[s[0]]
+                self.__options__.pop(s[0],None)
 
+        print(f"Winner:\n{winner[0]}, {winner[1]}", file=self.result_file)
         return winner
 
-def count_votes(participants : int, votes : List[Vote], options : List[Option]) -> Tuple[Option, int]:
-    turn = Turn(participants,votes,options)
+def count_votes(participants : int, votes : List[Vote], options : List[Option], fs : TextIO = None ) -> Tuple[Option, int]:
+    turn = Turn(participants,votes,options, fs)
     return turn.count()
