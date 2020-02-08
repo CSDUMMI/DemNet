a250b544ddda58099e473ddb510210505b46c924
Joris Gutjahr <joris.gutjahr@protonmail.com>
2020-02-08 11:04:14 +0100

    Documentation for .el File Format

---
+++ Server/ELECTION.el
@@ -0,0 +1,8 @@
+# File to represent an election result
+
+Votes:
+# List of votes as python lists
+Thrown:
+# All the options, that were thrown out after each other
+Winner:
+# The Option, that won + the number of votes in support of it.
