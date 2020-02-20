059d81b8d59965594527218c03ddd98de2b5175a
Joris Gutjahr <joris.gutjahr@protonmail.com>
2020-02-20 20:35:25 +0100

    manage.py is now either going to run the server or run a test. Updated start, removing unnecessary build instructions for Elm.

--- manage.py
+++ manage.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
-import sys, subprocess
+import sys, subprocess, os
 
 
-if sys[1] == "test":
+if sys.argv[1] == "test":
     subprocess.run("pytest")
+elif sys.argv[1] == "run":
+    os.environ["BUILDER"] = sys.argv[2]
+    subprocess.run("./start")
+else:
+    print("""Please provide a command:
+test : To run the tests
+run : To run the Server""", file=sys.stderr)
