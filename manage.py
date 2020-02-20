cc9525ef8068f8a610d4f41ead13d397c2272e3b
Joris Gutjahr <joris.gutjahr@protonmail.com>
2020-02-20 20:52:33 +0100

    Changed way of executing pytest.

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import sys, subprocess, os
 
 
 if sys.argv[1] == "test":
-    exit(subprocess.run("pytest").returncode)
+    exit(subprocess.run("python3 -m pytest").returncode)
 elif sys.argv[1] == "run":
     os.environ["BUILDER"] = sys.argv[2]
     exit(subprocess.run("./start").returncode)
