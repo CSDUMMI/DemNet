569668d3a3185356a09e61cdfe30d861cbd91a00
Joris Gutjahr <joris.gutjahr@protonmail.com>
2020-03-09 10:22:55 +0100

    Removing Database definitions (peewee classes) in main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, url_for, redirect, request
-from peewee import *
 
 from Crypto.Hash import SHA256
 
@@ -7,6 +6,9 @@ from functools import wraps
 from typing import List
 import datetime
 
+from peewee import *
+from Server.Database import *
+ 
 SECRET_KEY  = os.environ["SECRET_KEY"]
 DEBUG       = "DEBUG" in os.environ
 DATABASE    = os.environ["DATABASE"]
@@ -14,83 +16,6 @@ DATABASE    = os.environ["DATABASE"]
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-database    = SqliteDatabase(DATABASE)
-
-class BaseModel(Model):
-    class Meta():
-        database    = database
-
-class User(BaseModel):
-    name        = CharField(unique=True)
-    id          = CharField(unique=True)
-    password    = CharField()
-    salt        = CharField()
-
-    def publish(self, title : str, content : str):
-        Message.create  ( author    = self
-                        , title     = title
-                        , content   = content
-                        )
-        return True
-
-    def can_authenticate(self, password : str):
-        password = SHA256.new()
-            .update(password.encode("utf-8") + self.salt.encode("utf-8"))
-            .hexdigest()
-        if password == self.password:
-            return True
-        else:
-            return False
-
-    def vote(self, election : Election, choice : List[str]):
-        if Participant.select().where(user == self).count() == 0:
-            Vote.create ( election = election
-                        , choice = json.dumps(choice)
-                        )
-            Participant.create  ( election  = election
-                                , user      = self
-                                )
-            return True
-        else:
-            return False
-
-    def propose(self, election : Election, title : str, patch : str, conventional : bool):
-        Proposal.create ( author        = self
-                        , election      = election
-                        , title         = title
-                        , patch         = patch
-                        , conventional  = conventional
-                        )
-
-
-class Election(BaseModel):
-    id                      = IntegerField(unique=True, index=True, primary_key=True)
-    title                   = TextField()
-    description             = TextField()
-    creation_date           = DateField()
-    openning_ballot_date    = DateField()
-    closing_date            = DateField()
-
-class Vote(BaseModel):
-    election                = ForeignKeyField(Elections, backref="votes")
-    choice                  = TextField()
-
-class Participant(BaseModel):
-    election                = ForeignKeyField(Election, backref="participants")
-    user                    = ForeignKeyField(User, backref="participation")
-
-class Proposal(BaseModel):
-    election                = ForeignKeyField(Election, backref="proposals")
-    author                  = ForeignKeyField(User, backref="proposals")
-    title                   = CharField()
-    patch                   = TextField()
-    conventional            = BooleanField()
-
-class Message(BaseModel):
-    author                  = ForeignKeyField(User, backref="messages")
-    title                   = TextField()
-    content                 = TextField()
-    publishing_date         = DateTimeField()
 
 # Routes used by the average users:
 def login_required(f):
